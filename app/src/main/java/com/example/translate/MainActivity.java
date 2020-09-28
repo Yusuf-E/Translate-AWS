09aff9b675a61256ba5e8799c344a4a8976749d5
Yusuf EFE <65026569+EFE-Yusuf@users.noreply.github.com>
2020-09-28 15:24:18 +0300

    Fragments added

--- app/src/main/java/com/example/translate/MainActivity.java
+++ app/src/main/java/com/example/translate/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.translate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.hitomi.cmlibrary.CircleMenu;
 import com.hitomi.cmlibrary.OnMenuSelectedListener;
 import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
@@ -21,7 +25,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initBottomNavView();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new HomeFragment()).commit();
 
     }
-
+        private BottomNavigationView.OnNavigationItemSelectedListener navlistener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        selectedFragment=new HomeFragment();
+                        break;
+                    case R.id.nav_learn:
+                        selectedFragment =new LearnFragment();
+                        break;
+                    case R.id.nav_words:
+                        selectedFragment =new WordsFragment();
+                        break;
+                    case R.id.nav_saved:
+                        selectedFragment=new SaveFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,selectedFragment).commit();
+                return true;
+            }
+        };
+    private void initBottomNavView() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navlistener);
     }
+
+}
