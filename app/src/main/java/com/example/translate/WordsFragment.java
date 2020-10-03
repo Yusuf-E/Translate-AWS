1286cde3e0c371019763bc89b5db1b188edf805d
Yusuf EFE <65026569+EFE-Yusuf@users.noreply.github.com>
2020-10-03 19:01:05 +0300

    other sections design changed

--- app/src/main/java/com/example/translate/WordsFragment.java
+++ app/src/main/java/com/example/translate/WordsFragment.java
@@ -4,15 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class WordsFragment extends Fragment {
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_words,container,false);
+    public View onCreateView( LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.fragment_words,container,false);
+
+        ArrayList<String> dizi = new ArrayList<>();
+        dizi.add("");
+        dizi.add("item1");
+        dizi.add("item2");
+        dizi.add("item3");
+        dizi.add("item1");
+        dizi.add("item2");
+        dizi.add("item3");
+        dizi.add("item1");
+        dizi.add("item2");
+        dizi.add("item3");
+        dizi.add("item1");
+        dizi.add("item2");
+        dizi.add("item3");
+        dizi.add("item1");
+        dizi.add("item2");
+        dizi.add("item3");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,dizi);
+
+
+        ListView listView = ((ListView) v.findViewById(R.id.listview));
+
+        listView.setAdapter(adapter);
+        return v;
     }
 }
