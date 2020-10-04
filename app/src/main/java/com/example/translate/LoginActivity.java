b3c39538b5dea4e992789bcd9d42fb93da4f279b
Yusuf EFE <65026569+EFE-Yusuf@users.noreply.github.com>
2020-10-04 17:46:17 +0300

    feat: Login screen added to project

---
+++ app/src/main/java/com/example/translate/LoginActivity.java
@@ -0,0 +1,81 @@
+package com.example.translate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText email, password;
+    Button login;
+    TextView register;
+    boolean isEmailValid, isPasswordValid;
+    TextInputLayout emailError, passError;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        register = (TextView) findViewById(R.id.register);
+        emailError = (TextInputLayout) findViewById(R.id.emailError);
+        passError = (TextInputLayout) findViewById(R.id.passError);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SetValidation();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect to RegisterActivity
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void SetValidation() {
+        // Check for a valid email address.
+        if (email.getText().toString().isEmpty()) {
+            emailError.setError(getResources().getString(R.string.email_error));
+            isEmailValid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
+            emailError.setError(getResources().getString(R.string.error_invalid_email));
+            isEmailValid = false;
+        } else  {
+            isEmailValid = true;
+            emailError.setErrorEnabled(false);
+        }
+
+        // Check for a valid password.
+        if (password.getText().toString().isEmpty()) {
+            passError.setError(getResources().getString(R.string.password_error));
+            isPasswordValid = false;
+        } else if (password.getText().length() < 6) {
+            passError.setError(getResources().getString(R.string.error_invalid_password));
+            isPasswordValid = false;
+        } else  {
+            isPasswordValid = true;
+            passError.setErrorEnabled(false);
+        }
+
+        if (isEmailValid && isPasswordValid) {
+            Toast.makeText(getApplicationContext(), "Successfully", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    }
